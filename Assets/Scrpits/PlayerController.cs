f6ca4314ed9f28f8ddf3d5de56e32bd398316fea
Barış Kıral <bariskiral07@gmail.com>
2020-07-30 02:18:24 +0300

    30/07 02:00 AM

--- Assets/Scrpits/PlayerController.cs
+++ Assets/Scrpits/PlayerController.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private InputControls inputControls;
+    private Rigidbody2D rb;
+    private Collider2D col;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpSpeed = 7f;
+    [SerializeField] private LayerMask ground;
+
+    private void Awake()
     {
-        
+        inputControls = new InputControls();
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        inputControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControls.Disable();    
+    }
+
+    void Start()
+    {
+        inputControls.Player.Jump.performed += ctx => Jump();
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector2 topLeftPoint = transform.position;
+        topLeftPoint.x -= col.bounds.extents.x;
+        topLeftPoint.y += col.bounds.extents.y;
+
+        Vector2 bottomLeftPoint = transform.position;
+        bottomLeftPoint.x += col.bounds.extents.x;
+        bottomLeftPoint.y -= col.bounds.extents.y;
+
+        return Physics2D.OverlapArea(topLeftPoint, bottomLeftPoint, ground);
+    }
+
+
     void Update()
     {
-        
+        float moveInput = inputControls.Player.Move.ReadValue<float>();
+        rb.velocity = new Vector2(moveInput*moveSpeed, rb.velocity.y);
+
     }
+
 }
